8de63454e970940139902e641e978a927da8b7f7
letrend <simontrendel@gmail.com>
2022-12-22 23:54:13 +0100

    with midi send from pressure measurements

--- src/cyclone_beats/cyclone_beats.ino
+++ src/cyclone_beats/cyclone_beats.ino
@@ -1,3 +1,11 @@
+/*
+ * find midi device port with: 
+ * aseqdump -l
+ * then listen to midi device:
+ * aseqdump -p 24:0
+ */
+
+
 #include <Wire.h>
 #include "ClosedCube_TCA9546A.h"
 #include <Dps310.h>
@@ -18,10 +26,72 @@
 USBMIDI_Interface midi;
 //USBDebugMIDI_Interface midi{115200};
 
+#include <Control_Surface.h>
+
+float pressure[8] = {0};
+float pressure_mean[8] = {0};
+
+BEGIN_CS_NAMESPACE
+
+/*
+ * @brief   A class for momentary buttons and switches that send MIDI events.
+ *
+ * The button is debounced, and the internal pull-up resistor is enabled.
+ *
+ * @see     NoteButton
+ * @see     Button
+ */
+class MyNoteButton : public MIDIOutputElement {
+ public:
+  /*
+   * @brief   Create a new MyNoteButton object on the given pin, with the 
+   *          given address and velocity.
+   * 
+   * @param   pin
+   *          The digital input pin to read from.  
+   *          The internal pull-up resistor will be enabled.
+   * @param   address
+   *          The MIDI address to send to.
+   * @param   velocity
+   *          The MIDI note velocity [0, 127].
+   */
+  MyNoteButton(pin_t pin, MIDIAddress address, uint8_t velocity)
+    : sensor(pin), address(address), velocity(velocity) {}
+
+ public:
+  // Initialize: enable the pull-up resistor for the button
+  // This method is called once by `Control_Surface.begin()`.
+  void begin() final override {  }
+
+  // Update: read the button and send MIDI messages when appropriate.
+  // This method is called continuously by `Control_Surface.loop()`.
+  void update() final override {
+    uint8_t vel = min(pressure[sensor]-pressure_mean[sensor],255);
+    if ( pressure[sensor]>pressure_mean[sensor] ) {               // if pressed
+      Control_Surface.sendNoteOn(address, vel);  //   → note on
+    } else if (pressure[sensor]<pressure_mean[sensor]) {         // if released
+      Control_Surface.sendNoteOff(address, vel); //   → note off
+    }
+  }
+
+ private:
+  int sensor;
+  const MIDIAddress address;
+  uint8_t velocity;
+};
+
+END_CS_NAMESPACE
+
 // Instantiate a NoteButton object
-NoteButton button {
-  KNOB_SW,                       // Push button on pin 5
-  {MIDI_Notes::C(4), CHANNEL_1}, // Note C4 on MIDI channel 1
+MyNoteButton midi_sensors[] {
+  {0, {MIDI_Notes::C(4), CHANNEL_1},0},
+  {1, {MIDI_Notes::D(4), CHANNEL_1},0},
+  {2, {MIDI_Notes::E(4), CHANNEL_1},0},
+  {3, {MIDI_Notes::E(4), CHANNEL_1},0},
+  {4, {MIDI_Notes::G(4), CHANNEL_1},0},
+  {5, {MIDI_Notes::A(4), CHANNEL_1},0},
+  {6, {MIDI_Notes::A(4), CHANNEL_1},0},
+  {7, {MIDI_Notes::C(5), CHANNEL_1},0}
 };
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_RGBW + NEO_KHZ800);
@@ -29,8 +99,6 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_RGBW + NEO_KHZ8
 ClosedCube_TCA9546A tca9546a;
 
 Dps310 sensors[8];
-float pressure_mean[8] = {0};
-int8_t oversampling = 0;
 
 //uint8_t pressure_count = 0, temperature_count = 0;
 //float pressure[32], temperature[32];
@@ -53,8 +121,6 @@ void setup()
   Serial.begin(115200);
 	
 	tca9546a.begin(0x71);
- 
-  float pressure;
 
   int j = 0;
   for(int i=0;i<4;i++){
@@ -62,7 +128,7 @@ void setup()
     sensors[j].begin(Wire, 0x76);
 //    int16_t ret = sensors[j].startMeasurePressureOnce(prs_mr, prs_osr);
     sensors[j].correctTemp();
-    int ret = sensors[j].measurePressureOnce(pressure,0);
+    int ret = sensors[j].measurePressureOnce(pressure[j],0);
     Serial.print("sensor \t");
     Serial.print(j);
     if (ret != 0){
@@ -76,7 +142,7 @@ void setup()
     sensors[j].begin(Wire, 0x77);
 //    ret = sensors[j].startMeasurePressureCont(prs_mr, prs_osr);
     sensors[j].correctTemp();
-    ret = sensors[j].measurePressureOnce(pressure,0);
+    ret = sensors[j].measurePressureOnce(pressure[j],0);
     Serial.print("sensor \t");
     Serial.print(j);
     if (ret != 0){
@@ -98,8 +164,8 @@ void setup()
   do{
     t1 = millis();
     for(int sensor = 0; sensor<8; sensor++){
-      float pressure = measure(sensor);
-      pressure_mean[sensor] += pressure;
+      pressure[sensor] = measure(sensor);
+      pressure_mean[sensor] += pressure[sensor];
       double counter = (t1-t0)/3000.0;
       pixels.setPixelColor(PIXEL(sensor), pixels.Color((255-(1.0-counter)*255),0,0,0));
     }
@@ -118,8 +184,8 @@ void setup()
 void loop()
 {
 	for(int sensor = 0; sensor<8; sensor++){
-    float pressure = measure(sensor);
-    float diff = pressure - pressure_mean[sensor];
+    pressure[sensor] = measure(sensor);
+    float diff = pressure[sensor] - pressure_mean[sensor];
     if(diff < -DEADBAND){
       uint8_t white = min((-diff-DEADBAND)*SENSITIVITY,255);
       pixels.setPixelColor(PIXEL(sensor), pixels.Color(0,0,0,white));
@@ -129,11 +195,6 @@ void loop()
       uint8_t blue = min((diff-DEADBAND)*SENSITIVITY,255);
       pixels.setPixelColor(PIXEL(sensor), pixels.Color(0,0,blue,0));
     }
-//    pixels.setPixelColor(sensor, pixels.Color(0,0,0,0));
-//    Serial.print(pressure_mean[sensor]); 
-//    Serial.print( "\t"); 
-//    Serial.println(diff); 
-    
 	}
   pixels.show(); // This sends the updated pixel color to the hardware.
   Control_Surface.loop(); // Update the Control Surface
